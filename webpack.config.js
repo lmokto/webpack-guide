33061f727bacff1dc08ab4d5c38a9891d5d388f8
Luis Uranga <lurangar@everis.com>
2020-09-06 20:26:05 +0200

    Update repository with first steps according webpack guide

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
+  mode: 'development',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+   filename: 'bundle.js',
+   filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
