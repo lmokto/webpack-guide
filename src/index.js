035ad51e4fd5dfff8f2b150a09f78229aef403fd
Luis Uranga <lurangar@everis.com>
2020-09-06 21:23:19 +0200

    Update environment and build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import numRef from './ref.json';
+
+export function numToWord(num) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.num === num ? ref.word : accum;
+  }, '');
+}
+
+export function wordToNum(word) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.word === word && word.toLowerCase() ? ref.num : accum;
+  }, -1);
+}
 
 async function getAsyncComponent() {
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,54 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js',
-  },
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  optimization: {
-    moduleIds: 'hashed',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+// npx webpack --env.NODE_ENV=local --env.production --progress
+
+module.exports = env => {
+  // Use env.<YOUR VARIABLE> here:
+  console.log('NODE_ENV: ', env.NODE_ENV); // 'local'
+  console.log('Production: ', env.production); // true
+
+  return {
+    entry: {
+      app: './src/index.js',
+      print: './src/print.js',
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist',
+    },
+    optimization: {
+      moduleIds: 'hashed',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
         },
-      },
-    },    
-  },  
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all',
-  //   },
-  // },  
-  plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new HtmlWebpackPlugin({
-      title: 'Caching',
-    }),
-  ],
-  output: {
-    // filename: '[name].bundle.js',
-    // chunkFilename: '[name].bundle.js',
-    // publicPath: '/',
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
+      },    
+    },  
+    // optimization: {
+    //   splitChunks: {
+    //     chunks: 'all',
+    //   },
+    // },  
+    plugins: [
+      new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+      new HtmlWebpackPlugin({
+        title: 'Caching',
+      }),
+    ],
+    output: {
+      // filename: '[name].bundle.js',
+      // chunkFilename: '[name].bundle.js',
+      // publicPath: '/',
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+  }
+
 };
